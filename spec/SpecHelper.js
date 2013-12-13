e9577803dc0e0385fe0810ec26890fab376e03c2
david viramontes <dviramontes@gmail.com>
2013-12-13 01:36:34 -0700

    adds first two specs

---
+++ spec/SpecHelper.js
@@ -0,0 +1,11 @@
+var colors = require("colors");
+var _ = require("lodash-node");
+
+global.libs = {}; // global object , placeholder
+global.colors = colors;
+global._ = _;
+
+beforeEach(function(){
+	console.log("running test spec helper".blue);
+	libs.midi = require('midi');
+})
---
+++ spec/midiSpec.js
@@ -0,0 +1,19 @@
+var colors = require("colors");
+var _ = require("lodash-node");
+
+describe("midi library loaded", function() {
+    it("should be able to create new midi function", function() {
+        expect(libs.midi).toBeDefined();
+    });
+});
+
+describe("there should be an instrument connected", function() {
+        it("should be called Launchpad", function() {
+                var midi = libs.midi;
+                var output = new midi.output();
+                expect(output.getPortName(1)).toBe("Launchpad");
+                // _.times(output.getPortCount(), function(n) {
+
+                // });
+            });
+        });
