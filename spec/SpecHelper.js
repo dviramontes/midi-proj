fecdd2aa0e3424b1ab23989980eda9f9ce8e3bf1
david viramontes <dviramontes@gmail.com>
2013-12-13 01:38:33 -0700

    removes globals in specHelper.js

--- spec/SpecHelper.js
+++ spec/SpecHelper.js
@@ -2,8 +2,8 @@ var colors = require("colors");
 var _ = require("lodash-node");
 
 global.libs = {}; // global object , placeholder
-global.colors = colors;
-global._ = _;
+// global.colors = colors;
+// global._ = _;
 
 beforeEach(function(){
 	console.log("running test spec helper".blue);
